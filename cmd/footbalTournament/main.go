85ee9c542935122f73cc5f714404e25f7c783a42
Levshakov Nikolai <levshakov_nikk@bk.ru>
2022-06-13 22:23:21 +0300

    Change makefile, added a few constants and first attempt in parsing the hmtl page

--- cmd/footbalTournament/main.go
+++ cmd/footbalTournament/main.go
@@ -1,9 +1,57 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
+	"net/http"
+
+	"golang.org/x/net/html"
+)
+
+// + results
+// + standings
+// + fixtures
+
+const (
+	urlMain   = "https://www.flashscore.com"
+	urlEPL    = "england/premier-league/"
+	urlL1     = "france/ligue-1/"
+	urlBUNDES = "germany/bundesliga/"
+	urlSA     = "italy/serie-a/"
+	urlRPL    = "russia/premier-league/"
+)
+
+var (
+	EPLTeams    = []string{"Manchester City", "Liverpool", "Chelsea", "Arsenal", "Manchester United", "Tottenham"}
+	L1Teams     = []string{"Paris SG", "Monaco"}
+	BUNDESTeams = []string{"RB Leipzig", "Dortmund", "Bayern Munich"}
+	SATeams     = []string{"AC Milan", "Inter", "Napoli", "Atalanta", "AS Roma"}
+	RPLTeams    = []string{"CSKA Moscow", "Spartak Moscow", "Krasnodar", "Lokomotiv Moscow", "Zenit"}
 )
 
 func main() {
-  fmt.Println("Hello World!")
+	resp, err := http.Get(urlMain)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+	//	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, a := range n.Attr {
+				if a.Key == "href" {
+					fmt.Println(a.Val)
+					break
+				}
+			}
+		}
+	}
+	f(doc)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module footbalTournament.com
 
 go 1.16
+
+require golang.org/x/net v0.0.0-20220607020251-c690dde0001d // indirect
